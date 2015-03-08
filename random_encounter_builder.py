61bec598ddb75fb5599d4ce2db7af12bd2db9864
Zachary Hill <zhill@octonetwork.com>
2015-03-07 20:16:31 -0500

    Fixed bug where build_encounter generated empty list

--- random_encounter_builder.py
+++ random_encounter_builder.py
@@ -77,6 +77,7 @@ xp_per_monster = xp_list_gen(encounter_xp)
 output_monster = build_encounter(xp_per_monster[1])
 
 
+# TODO add cycle effect so you can continually iterate your random selection based on random factor already generated
 print('Randomized encounter based on:\nParty Size: {0}\nParty Level: {1}\n'
       'Difficulty: {2}\n'
       '{3}x {4}(s) found on Monster Manual page: {5}'.format(
